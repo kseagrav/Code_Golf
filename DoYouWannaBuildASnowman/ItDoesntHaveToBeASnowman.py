9a97752ca864b9f1d47d60359b6f7f982b474f54
Kayleigh <beautifullyawkward@msn.com>
2015-10-05 16:34:35 -0400

    Compressed version, with testing functionality in a separate file. -- Doing this because the last commit message was bad; result of getting to know command line instead of using the GUI like I always have.

--- DoYouWannaBuildASnowman/ItDoesntHaveToBeASnowman.py
+++ DoYouWannaBuildASnowman/ItDoesntHaveToBeASnowman.py
@@ -5,7 +5,7 @@ Created on Mon Oct 05 15:37:48 2015
 @author: kseagraves
 """
 
-from DoYouWannaBuildASnowman import print_snowman
+from DoYouWannaBuildASnowman import snowman as print_snowman
 
 # Test with different patterns
 print_snowman("11114411")
