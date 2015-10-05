b483b83b95a8b7896bbee9ef4ae13fb69c74f4db
Kayleigh <beautifullyawkward@msn.com>
2015-10-05 16:27:23 -0400

    Compressed version, with testing functionality in a separate file.

---
+++ DoYouWannaBuildASnowman/ItDoesntHaveToBeASnowman.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 05 15:37:48 2015
+
+@author: kseagraves
+"""
+
+from DoYouWannaBuildASnowman import print_snowman
+
+# Test with different patterns
+print_snowman("11114411")
+print_snowman("33232124")
+print_snowman("14441133")
+print_snowman("22323242")
+print_snowman("11111343")
