c0c414d820d13f3feac1e75005975b267cccbedf
Kayleigh Bray <kayleigh.bray@cadre5.com>
2019-02-12 21:38:21 -0500

    Uglify round 1: replace variables

--- TwoEggProblem/Program.cs
+++ TwoEggProblem/Program.cs
@@ -1,51 +1,44 @@
 ﻿using System;
 
-class TwoEggProblem
+class T
 {
-    static void Main(string[] args)
+    static void Main(string[] a)
     {
-        int numFloors = int.Parse(args[0]);
-        int breakFloor = int.Parse(args[1]);
+        int n = int.Parse(a[0]);
+        int b = int.Parse(a[1]);
 
-        int stepSize = (Convert.ToInt32(Math.Sqrt(1 + (8 * numFloors))) - 1) / 2;
-        int remainder = (numFloors - ((stepSize * (stepSize + 1)) / 2));
-        int lastGoodFloor = 0;
-        int earliestBreak = numFloors + 1;
-        bool repFlag = false;
+        int s = (Convert.ToInt32(Math.Sqrt(1 + (8 * n))) - 1) / 2;
+        int r = (n - ((s * (s + 1)) / 2));
+        int l = 0;
+        int e = n + 1;
+        bool f = false;
 
-        // traverse the building in leaps that are shrinking in size as we go up
-        for(int tryLeap = stepSize; tryLeap < numFloors; tryLeap += stepSize) {
-            Console.WriteLine(tryLeap);
-            bool doesBreak = tryLeap >= breakFloor;
-            Console.WriteLine(doesBreak);
-            if (doesBreak) {
-                earliestBreak = tryLeap;
+        for(int t = s; t < n; t += s) {
+            Console.WriteLine(t);
+            Console.WriteLine(t >= b);
+            if (t >= b) {
+                e = t;
                 break;
             }
-            lastGoodFloor = tryLeap;
+            l = t;
 
-            // handle the one case where you need to keep the step size constant
-            if ((stepSize == remainder) && !repFlag) {
-                repFlag = true;
+            if ((s == r) && !f) {
+                f = true;
             } else {
-                --stepSize;
+                --s;
             }
         }
 
-        // we found a floor on which the floor breaks, go to immediately above the
-        // last floor that it did *not* break on and go 1 at a time until we hit
-        // the earliest floor at which it breaks
-        for (int tryFloor = (lastGoodFloor + 1); tryFloor < earliestBreak; ++tryFloor) {
-            Console.WriteLine(tryFloor);
-            bool doesBreak = tryFloor >= breakFloor;
-            Console.WriteLine(doesBreak);
-            if (doesBreak) {
-                earliestBreak = tryFloor;
+        for (int t = (l + 1); t < e; ++t) {
+            Console.WriteLine(t);
+            Console.WriteLine( t >= b);
+            if ( t >= b) {
+                e = t;
                 break;
             }
         }
 
-        Console.WriteLine("Floor " + earliestBreak);
+        Console.WriteLine("Floor " + e);
     }
 }
 
